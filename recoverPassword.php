550868f5437496d5e384e9972e8ecc82e2c4dcb3
Johnny <joaofeirense@gmail.com>
2018-11-09 00:46:43 -0300

    recover password

---
+++ recoverPassword.php
@@ -0,0 +1,28 @@
+<?php
+include('connect.php');
+if (isset($_POST['email']))
+{
+  $email = $_POST['email'];
+  $query = "SELECT * FROM `user` WHERE email = '$email'";
+  $rslt = $connection->query($query);
+
+  if (mysqli_num_rows($rslt)>0)
+  {
+    $fetch = $rslt->fetch_assoc();
+    $psswd = $fetch['psswd'];
+    $to = $email;
+    $subject = "Recover Password";
+    $message = "your password is $psswd";
+    $from = "me@freakazoid.com";
+
+    if (mail($to,$subject,$message,$from))
+    {
+      echo "mesagem enviada com exito";
+    }
+    else
+    {
+      echo "deu ruim";
+    }
+  }
+}
+ ?>
